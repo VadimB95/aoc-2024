0d57fd82f4d37f548fb96a36420e6b8f6424ab4a
v.babikov <v.babikov@tinkoff.ru>
2024-12-08 21:34:08 +0400

    add day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,21 +1,45 @@
+import kotlin.math.absoluteValue
+
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
-
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
-
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
-    // Or read a large test input from the `src/Day01_test.txt` file:
+    // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 11)
+    check(part2(testInput) == 31)
 
-    // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
-    part1(input).println()
-    part2(input).println()
+    println(part1(input))
+    println(part2(input))
+}
+
+private fun part1(input: List<String>): Int {
+    val a = mutableListOf<Int>()
+    val b = mutableListOf<Int>()
+    input.forEach { line ->
+        val split = line.split("   ")
+        a.add(split[0].toInt())
+        b.add(split[1].toInt())
+    }
+    a.sort()
+    b.sort()
+    var sum = 0
+    for (i in a.indices) {
+        sum += (a[i] - b[i]).absoluteValue
+    }
+    return sum
+}
+
+private fun part2(input: List<String>): Int {
+    val a = mutableListOf<Int>()
+    val b = mutableListOf<Int>()
+    input.forEach { line ->
+        val split = line.split("   ")
+        a.add(split[0].toInt())
+        b.add(split[1].toInt())
+    }
+    val bFrequency = b.groupingBy { it }.eachCount()
+    var sum = 0
+    a.forEach {
+        sum += it * (bFrequency[it] ?: 0)
+    }
+    return sum
 }
